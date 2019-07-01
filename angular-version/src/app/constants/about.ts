716a5ea4d86d2764c549a0543f6c9227ee4c1e44
Krasen Ilkov <sillimanite90@gmail.com>
2019-07-01 13:15:47 +0300

    initial layout

--- angular-version/src/app/constants/about.ts
+++
@@ -1,6 +0,0 @@
-export class About {
-    public static readonly me = {
-        'quote': 'Curiosity is followed by ambition',
-        'summary': 'Hello, my name is Krasen Ilkov, I\'m 27 years old Web Developer.I love computer technologies and everything that brings with them.I work with JavaScript both in front - end with libraries and frameworks like Angular 2, and I work on back-end with Express framework.I like to stay up - to - date with the new technology and to experiment with them to uderstarnd how they work and how can improve my workflow.'
-    };
-}
